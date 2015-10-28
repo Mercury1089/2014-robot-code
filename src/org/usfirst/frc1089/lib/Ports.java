3778e79a1bf744f5854dd05cc43e0e5b5d333ca5
Rajan Patel <compgeek97@gmail.com>
2015-10-28 17:30:03 -0400

    cleaned up Ports.java

--- src/org/usfirst/frc1089/lib/Ports.java
+++ src/org/usfirst/frc1089/lib/Ports.java
@@ -11,7 +11,7 @@ public class Ports {
                             DRIVE_LEFT_ENCODER_2 = 4,       //DIO
                             DRIVE_RIGHT_ENCODER_1 = 5,      //DIO
                             DRIVE_RIGHT_ENCODER_2 = 6;      //DIO
-    
+
     public static final int SHOOTER_LAUNCHER = 6,           //PWM
                             SHOOTER_ENCODER_1 = 1,          //DIO
                             SHOOTER_ENCODER_2 = 2,          //DIO
@@ -20,10 +20,10 @@ public class Ports {
                             SHOOTER_ANGLE_CONTROL = 5,      //PWM
                             SHOOTER_BRAKE_1 = 3,            //Sol
                             SHOOTER_BRAKE_2 = 4;            //Sol
-    
+
     public static final int COMPRESSOR_PRESSURE_SWITCH = 8, //DIO
                             COMPRESSOR_RELAY = 1;           //Rel
-    
+
     public static final int LOADER_INTAKE_ROLLER = 7,       //PWM
                             LOADER_EXTENDER_1 = 1,          //Sol
                             LOADER_EXTENDER_2 = 2;          //Sol
